c5ad82190a67f0958bd64b5aaf00130ee0479712
Bianca Bărăscu <126165357+BiancaBb01@users.noreply.github.com>
2024-03-06 20:42:01 +0200

    Create student.cpp

---
+++ LAB2/ex2/student.cpp
@@ -0,0 +1,43 @@
+#include "student.h"
+
+Student::Student(const std::string& name)
+    : name_(name), mathGrade_(0), englishGrade_(0), historyGrade_(0) {}
+
+void Student::setName(const std::string& name) {
+    name_ = name;
+}
+
+std::string Student::getName() const {
+    return name_;
+}
+
+void Student::setMathGrade(float grade) {
+    if (grade >= 1 && grade <= 10)
+        mathGrade_ = grade;
+}
+
+float Student::getMathGrade() const {
+    return mathGrade_;
+}
+
+void Student::setEnglishGrade(float grade) {
+    if (grade >= 1 && grade <= 10)
+        englishGrade_ = grade;
+}
+
+float Student::getEnglishGrade() const {
+    return englishGrade_;
+}
+
+void Student::setHistoryGrade(float grade) {
+    if (grade >= 1 && grade <= 10)
+        historyGrade_ = grade;
+}
+
+float Student::getHistoryGrade() const {
+    return historyGrade_;
+}
+
+float Student::getAverageGrade() const {
+    return (mathGrade_ + englishGrade_ + historyGrade_) / 3;
+}
