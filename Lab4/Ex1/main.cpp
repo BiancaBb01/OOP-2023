18ff64dd1d4e605d0f03e24ded72941137bae116
Bianca Bărăscu <126165357+BiancaBb01@users.noreply.github.com>
2024-03-19 18:29:13 +0200

    Create main.cpp

---
+++ Lab4/Ex1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include "Sort.h"
+
+int main()
+{
+    Sort a(5);
+    Sort *b = new Sort(5);
+    ///delete b;
+    bool ascendent=false;
+    ///a.BubbleSort(ascendent);
+   /// a.Print();
+    return 0;
+}
