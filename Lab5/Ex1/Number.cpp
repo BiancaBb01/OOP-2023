17c92febd887c5a497e4d3f892c1a96b57bc4b14
Bianca Bărăscu <126165357+BiancaBb01@users.noreply.github.com>
2024-03-26 23:30:42 +0200

    Update Number.cpp

--- Lab5/Ex1/Number.cpp
+++ Lab5/Ex1/Number.cpp
@@ -1,176 +1,112 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include "Number.h"
+#include <iostream>
+#include <cstring>
 
-int val(char c)
-{
-    if (c >= '0' && c <= '9')
-        return (int)c - '0';
-    else
-        return (int)c - 'A' + 10;
+Number::Number(const char* value, int base) : base(base) {
+    this->value = new char[strlen(value) + 1];
+    strcpy(this->value, value);
+    digitsCount = strlen(value);
 }
 
-int todeci(char* str, int base)
-{
-    int len = strlen(str);
-    int pow = 1;
-    int num = 0;
-    int i;
-    for (i = len - 1; i >= 0; i--)
-    {
-        if (val(str[i]) >= base)
-        {
-            printf("Invalid Number");
-            return -1;
-        }
-        num += val(str[i]) * pow;
-        pow = pow * base;
-    }
-    return num;
+Number::Number(int value) : base(10) {
+    this->value = new char[20]; // assuming a max of 20 digits for base 10 numbers
+    sprintf(this->value, "%d", value);
+    digitsCount = strlen(this->value);
 }
 
-Number::Number(const char* newValue, int newBase)
-{
-    value = new char[32];
-    strcpy(value, newValue);
-    base = newBase;
+Number::Number(const Number& other) : base(other.base), digitsCount(other.digitsCount) {
+    value = new char[strlen(other.value) + 1];
+    strcpy(value, other.value);
 }
 
-Number::Number(const Number& n)
-{
-    base = n.base;
-    value = new char[32];
-    memcpy(value, n.value, 32);
+Number::Number(Number&& other) noexcept : value(other.value), base(other.base), digitsCount(other.digitsCount) {
+    other.value = nullptr;
 }
 
-Number::Number(int x)
-{
-    value = new char[32];
-    int aux = x, nrc = 0;
-    sprintf(value, "%d", aux);
-    base = 10;
-}
-
-Number::~Number()
-{
+Number::~Number() {
     delete[] value;
 }
 
-void Number::operator=(const Number& n)
-{
-    Number x(n.value, n.base);
-    x.SwitchBase(base);
-    base = x.base;
-    value = new char[32];
-    memcpy(value, x.value, 32);
+Number& Number::operator=(const Number& other) {
+    if (this != &other) {
+        delete[] value;
+        value = new char[strlen(other.value) + 1];
+        strcpy(value, other.value);
+        base = other.base;
+        digitsCount = other.digitsCount;
+    }
+    return *this;
 }
 
-void Number::operator=(const char* newValue)
-{
-    value = new char[32];
-    memcpy(value, newValue, 32);
+Number& Number::operator=(Number&& other) noexcept {
+    if (this != &other) {
+        delete[] value;
+        value = other.value;
+        other.value = nullptr;
+        base = other.base;
+        digitsCount = other.digitsCount;
+    }
+    return *this;
 }
 
-void Number::operator-()
-{
-    int aux = atoi(value);
-    aux = aux * (-1);
-    _itoa(aux, value, base);
+Number operator+(const Number& lhs, const Number& rhs) {
+    int base = (lhs.base > rhs.base) ? lhs.base : rhs.base;
+    // perform addition logic here
 }
 
-char& Number::operator[](int index)
-{
+Number operator-(const Number& lhs, const Number& rhs) {
+    int base = (lhs.base > rhs.base) ? lhs.base : rhs.base;
+    // perform subtraction logic here
+}
+
+char& Number::operator[](int index) {
     return value[index];
 }
 
-Number operator+(const Number& n1, const Number& n2)
-{
-    int Add, AddBase;
-    char str[32];
-    Number a(n1.value, n1.base);
-    Number b(n2.value, n2.base);
-    a.SwitchBase(10);
-    b.SwitchBase(10);
-    Add = atoi(a.value) + atoi(b.value);
-    sprintf(str, "%d", Add);
-    AddBase = max(a.base, b.base);
-    Number rez(str, 10);
-    rez.SwitchBase(AddBase);
-    return rez;
+bool Number::operator>(const Number& other) const {
+    // perform comparison logic here
 }
 
-Number Number::operator|(const Number& n)
-{
-    int pipe, pipeBase;
-    char str[64];
-    Number a(value, base);
-    pipeBase = max(a.base, n.base);
-    Number b(n.value, n.base);
-    if (a.base < b.base)
-    {
-        a.SwitchBase(b.base);
-    }
-    else {
-        b.SwitchBase(a.base);
-    }
-    strcpy(str, a.value);
-    strcat(str, b.value);
-    Number rez(str, pipeBase);
-    return rez;
+bool Number::operator<(const Number& other) const {
+    // perform comparison logic here
 }
 
-Number operator-(const Number& n1, const Number& n2)
-{
-    int Dif, DifBase;
-    char str[10];
-    Number a(n1.value, n1.base);
-    Number b(n2.value, n2.base);
-    a.SwitchBase(10);
-    b.SwitchBase(10);
-    Dif = atoi(a.value) - atoi(b.value);
-    sprintf(str, "%d", Dif);
-    DifBase = max(a.base, b.base);
-    Number rez(str, 10);
-    rez.SwitchBase(DifBase);
-    return rez;
+bool Number::operator>=(const Number& other) const {
+    // perform comparison logic here
 }
 
-bool Number::operator>(const Number& n1)
-{
-    bool G = 0;
-    Number a(value, base);
-    Number b(n1.value, n1.base);
-    a.SwitchBase(10);
-    b.SwitchBase(10);
-    if (atoi(a.value) > atoi(b.value))
-        G = 1;
-    return G;
+bool Number::operator<=(const Number& other) const {
+    // perform comparison logic here
 }
 
-Number operator+=(Number& n1, const Number& n2)
-{
-    n1 = n1 + n2;
-    return n1;
+bool Number::operator==(const Number& other) const {
+    // perform comparison logic here
 }
 
-void Number::SwitchBase(int newBase)
-{
-    int z;
-    z = todeci(value, base);
-    _itoa(z, value, newBase);
-    base = newBase;
+bool Number::operator!=(const Number& other) const {
+    // perform comparison logic here
 }
 
-void Number::Print()
-{
-    cout << value << '\n';
+void Number::SwitchBase(int newBase) {
+    // perform base conversion logic here
 }
 
-int  Number::GetDigitsCount()
-{
-    return strlen(value);
+void Number::Print() const {
+    std::cout << value << std::endl;
 }
 
-int  Number::GetBase()
-{
+int Number::GetDigitsCount() const {
+    return digitsCount;
+}
+
+int Number::GetBase() const {
     return base;
+}
+
+Number& Number::operator--() {
+    // perform decrement logic here
+}
+
+Number Number::operator--(int) {
+    // perform decrement logic here
 }
