9ddf3a60474b714111b289a3df9af4b3d253bfba
Bianca Bărăscu <126165357+BiancaBb01@users.noreply.github.com>
2024-04-07 21:52:25 +0300

    Update Seat.cpp

--- Lab6/Ex1/Seat.cpp
+++ Lab6/Ex1/Seat.cpp
@@ -1,6 +1,31 @@
 #include "Seat.h"
+#include "Car.h"
+#include "Weather.h"
 
-double Seat::calculateTime(int length, Weather weather) const {
-   
-    return 0.0;
+Seat::Seat() {
+	fuelCapacity = 50;
+	fuelConsumption = 30;
+	averageSpeed[Rain] = 40;
+	averageSpeed[Sunny] = 60;
+	averageSpeed[Snow] = 20;
+	name = (char*)"Seat";
+}
+
+char* Seat::getName() {
+	return name;
+}
+
+
+float Seat::Racing(bool& ok, int weather, int length) {
+	int speed = averageSpeed[weather]; // speed: 60
+	float cap = fuelCapacity / fuelConsumption; // cap:  5/3
+	float distance = cap * speed; // 1,6 * 60 => distance: 96
+
+	if (distance >= length) {
+		ok = true; // a terminat
+	}
+	else {
+		ok = false;
+	}
+	return (cap * length / distance);
 }
